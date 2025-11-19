9c98930a226723227fb90d849c69ea17db3b1abc
Julien Wan <julien.wan@aldes.com>
2025-11-19 22:25:10 +0100

    fix: bad imports

--- lib/forms/index.ts
+++ lib/forms/index.ts
@@ -1,6 +1,1 @@
-export * from './CheckboxField';
-export * from './EnumField';
 export * from './fields';
-export * from './SliderField';
-export * from './StringField';
-export * from './SwitchField';
